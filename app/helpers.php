fb6a0a3756a7d6c1a4c6d70bcf559cc78af6b287
pokujopo <pokujopo@gmail.com>
2026-04-16 18:11:52 +0300

    Fix moneyFormat to format millions correctly

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,7 @@
 
 function moneyFormat($number){
        if($number >= 1000000){
-          return round($number/1000000,1).'M';
+          return round($number/100000,1).'M';
        } elseif($number >= 1000){
         return round($number/1000,1).'K';
        }
