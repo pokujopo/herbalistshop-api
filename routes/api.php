cf9d4782b912adfb6a837faf05b9da8504d7f74f
pokujopo <pokujopo@gmail.com>
2026-09-02 03:12:26 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -123,6 +123,47 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/payments/{orderNumber}/status', [PaymentController::class, 'status']);
     Route::post('/payments/{orderNumber}/retry', [PaymentController::class, 'retry']);
 });
+
+Route::post('/yas/user/{refercode}', function ($refercode) {
+
+    $all_customer = [
+        "abc823" => [
+            "refer_code" => "abc823",
+            "customer_name" => "john doe",
+            "invitor_number" => 30000,
+        ],
+
+        "abc120" => [
+            "refer_code" => "abc120",
+            "customer_name" => "jo de",
+            "invitor_number" => 98000000000,
+        ],
+
+        "abc999" => [
+            "refer_code" => "abc999",
+            "customer_name" => "Test User",
+            "invitor_number" => 2340000000,
+        ],
+
+        "abc270" => [
+            "refer_code" => "abc270",
+            "customer_name" => "Te User",
+            "invitor_number" => 200000000000,
+        ],
+    ];
+
+    if (!isset($all_customer[$refercode])) {
+        return response()->json([
+            "status" => 404,
+            "message" => "Refercode not found",
+        ], 404);
+    }
+
+    return response()->json([
+        "status" => 200,
+        "customer_all" => $all_customer[$refercode],
+    ], 200);
+});
 /*
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
